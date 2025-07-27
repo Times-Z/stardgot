01adae2b56259bfda50c49c34dddec2fa931ea93
TimesZ <jonas.bertin49@gmail.com>
2025-07-27 13:19:31 +0200

    feat: wip the main menu & new game button works

--- Scripts/Menus/Main/MainMenu.cs
+++ Scripts/Menus/Main/MainMenu.cs
@@ -7,11 +7,19 @@ public partial class MainMenu : Control
     {
         GD.Print("MainMenu _Ready");
         var quitButton = GetNode<Button>("VBoxContainer/QuitButton");
+        var newGameButton = GetNode<Button>("VBoxContainer/NewGameButton");
+        newGameButton.Pressed += OnNewGameButtonPressed;
         quitButton.Pressed += OnQuitButtonPressed;
     }
 
     private void OnQuitButtonPressed()
     {
         GetTree().Quit();
+    }
+
+    private void OnNewGameButtonPressed()
+    {
+        GD.Print("New Game button pressed");
+        GetTree().ChangeSceneToFile("res://Scenes/MainMap.tscn");
     }
 }
