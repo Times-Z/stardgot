6a87bb2ab602bc6cf5057f25bd3e9fd7b228fbef
TimesZ <jonas.bertin49@gmail.com>
2025-07-27 20:50:33 +0000

    chore: refacto arbo

---
+++ scripts/menus/SettingsMenu.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+public partial class SettingsMenu : Control
+{
+	[Export] private NodePath BackButtonPath = "VBoxContainer/BackButton";
+
+	// Todo : need to be set in the editor but not fonctional yet
+	[Export] private string MainMenuScenePath = "res://scenes/menus/MainMenu.tscn";
+
+	public override void _Ready()
+	{
+		GD.Print("SettingsMenu _Ready");
+
+		var backButton = GetNode<Button>(BackButtonPath);
+
+		backButton.Pressed += OnBackButtonPressed;
+	}
+
+	private void OnBackButtonPressed()
+	{
+		if (!string.IsNullOrEmpty(MainMenuScenePath))
+		{
+			GetTree().ChangeSceneToFile(MainMenuScenePath);
+		}
+		else
+		{
+			GD.PrintErr("MainMenuScenePath is not assigned or is empty!");
+		}
+	}
+}
