582d45693d915404766b72c0e36820a97dab31fd
TimesZ <jonas.bertin49@gmail.com>
2025-07-27 18:08:18 +0200

    feat: disable texture filtering

--- Scripts/Menus/MenuMusicPlayer.cs
+++ Scripts/Menus/MenuMusicPlayer.cs
@@ -2,35 +2,34 @@ using Godot;
 
 public partial class MenuMusicPlayer : Node
 {
-    private AudioStreamPlayer _player;
-    [Export] public AudioStream MusicStream;
+	private AudioStreamPlayer _player;
+	[Export] public AudioStream MusicStream;
 
-    public override void _Ready()
-    {
-        GD.Print("MusicPlayer _Ready");
+	public override void _Ready()
+	{
+		GD.Print("MusicPlayer _Ready");
+		_player = new AudioStreamPlayer();
+		AddChild(_player);
+		if (MusicStream != null)
+		{
+			_player.Stream = MusicStream;
+			_player.Autoplay = true;
+			_player.Play();
+		}
+	}
 
-        _player = new AudioStreamPlayer();
-        AddChild(_player);
-        if (MusicStream != null)
-        {
-            _player.Stream = MusicStream;
-            _player.Autoplay = true;
-            _player.Play();
-        }
-    }
+	public void StopMusic()
+	{
+		_player?.Stop();
+	}
 
-    public void StopMusic()
-    {
-        _player?.Stop();
-    }
-
-    public void PlayMusic()
-    {
-        GD.Print("MusicPlayer PlayMusic");
-        if (_player != null && MusicStream != null)
-        {
-            _player.Stream = MusicStream;
-            _player.Play();
-        }
-    }
+	public void PlayMusic()
+	{
+		GD.Print("MusicPlayer PlayMusic");
+		if (_player != null && MusicStream != null)
+		{
+			_player.Stream = MusicStream;
+			_player.Play();
+		}
+	}
 }
